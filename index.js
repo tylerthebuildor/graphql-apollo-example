28cd1cbcc044fcf214679f880080f918ef831c0e
Tyler Buchea <tyler@buchea.com>
2021-02-04 10:43:23 -0800

    Update deps and fix nested query

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { ApolloServer, gql } = require('apollo-server');
-const jwt = require('jsonwebtoken');
+const { ApolloServer, gql } = require("apollo-server");
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = 'makethislongandrandom';
+const JWT_SECRET = "makethislongandrandom";
 
 // This could also be MongoDB, PostgreSQL, etc
 const db = {
   users: [
     {
-      organization: '123', // this is a relation by id
-      id: 'abc',
-      name: 'Elon Musk',
+      organization: "123", // this is a relation by id
+      id: "abc",
+      name: "Elon Musk",
     },
   ],
   organizations: [
     {
-      users: ['abc'], // this is a relation by ids
-      id: '123',
-      name: 'Space X',
+      users: ["abc"], // this is a relation by ids
+      id: "123",
+      name: "Space X",
     },
   ],
 };
@@ -25,7 +25,7 @@ const server = new ApolloServer({
   context: ({ req }) => {
     let user = null;
     try {
-      const token = req.headers.authorization.replace('Bearer ', '');
+      const token = req.headers.authorization.replace("Bearer ", "");
       user = jwt.verify(token, JWT_SECRET);
     } catch (error) {}
     return { user };
@@ -57,40 +57,41 @@ const server = new ApolloServer({
     Mutation: {
       signup(_, { organization, id, name }) {
         const user = { organization, id, name };
-        const match = db.users.find(user => user.name === name);
-        if (match) throw Error('This username already exists');
+        const match = db.users.find((user) => user.name === name);
+        if (match) throw Error("This username already exists");
         db.users.push(user);
         return user;
       },
     },
     Query: {
       login(_, { username }) {
-        const user = db.users.find(user => user.name === username);
+        const user = db.users.find((user) => user.name === username);
         if (!user) {
-          throw Error('username was incorrect');
+          throw Error("username was incorrect");
         }
         const token = jwt.sign({ id: user.id }, JWT_SECRET);
         return token;
       },
       tellMeADadJoke(_, data, { user }) {
-        if (!user) throw Error('not authorized');
-        return 'If you see a robbery at an Apple Store does that make you an iWitness?';
+        if (!user) throw Error("not authorized");
+        return "If you see a robbery at an Apple Store does that make you an iWitness?";
       },
       users: () => db.users,
-      user: (_, { id }) => db.users.find(user => user.id === id),
+      user: (_, { id }) => db.users.find((user) => user.id === id),
       organizations: () => db.organizations,
       organization: (_, { id }) =>
-        db.organizations.find(organization => organization.id === id),
+        db.organizations.find((organization) => organization.id === id),
     },
     User: {
-      organization: parent =>
-        db.organizations.find(({ id }) => parent.organizationId === id),
+      organization: (parent) => {
+        return db.organizations.find(({ id }) => parent.organization === id);
+      },
     },
     Organization: {
       async users(parent) {
-        await new Promise(resolve => setTimeout(resolve, 5000));
+        await new Promise((resolve) => setTimeout(resolve, 5000));
         const organization = db.users.filter(({ id }) =>
-          parent.userIds.includes(id)
+          parent.users.includes(id)
         );
         return organization;
       },
